e51de05a40480b416e90d55686d18244e4fade7d
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 11:39:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;
+  document.querySelector('input[type="button"]').addEventListener('click', function() {
+   
+    colorSelect.remove(selectedIndex);
+  });
